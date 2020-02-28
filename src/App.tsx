c7b85e2edde558ebea717e71a41f961760e3cdf8
Sven <60364068+at-Sven@users.noreply.github.com>
2020-02-28 15:35:34 +0100

    Prompt Nutzung: Versuch Nr. 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,13 @@ import axios from 'axios';
 import './App.css';
 
 import Coin from '../src/Coin';
+import CoinDetails from './CoinDetails';
 
 interface State {
   coins: Coin[];
+  coinID: string;
 }
-export default class App extends React.Component<{}, { coins: object[] }> {
-
-  constructor(props: any) {
-    super(props);
-    this.state = { coins: [] };
-  }
-
-
-
+export default class App extends React.Component<{}, State> {
 
   componentDidMount() {
     axios.get('https://api.coingecko.com/api/v3/coins')
@@ -24,7 +18,13 @@ export default class App extends React.Component<{}, { coins: object[] }> {
       })
   }
 
+  showCoinDetails(coinID: string, e: any) {
+    console.log(coinID + ", für spätere Aufrufe");
+    this.setState({ coinID: coinID });
+  };
+
   render() {
+    if (!this.state) { return null }
     return (
       <div className="App" >
         <header className="App-header">
@@ -33,48 +33,14 @@ export default class App extends React.Component<{}, { coins: object[] }> {
 
           <div className="content">
             {this.state.coins.map((coin: any) => (
-              <div key={coin.id} className="coinFrame">
+              <div key={coin.id} className="coinFrame"
+                onClick={(e) => this.showCoinDetails(coin.id, e)}>
                 <p> <img src={coin.image.thumb} alt="" /> {coin.name}</p>
               </div>
             ))}
           </div>
 
-          {this.state.coins.map((coin: any) => (
-            <div key={coin.id} className="coinDetail">
-              <div>
-                <img src={coin.image.large} alt="" />
-                <h1>{coin.name} ({coin.symbol})</h1>
-                <hr />
-                <h2>last update</h2>
-                <p>{new Date(coin.last_updated).toUTCString()}</p>
-                <hr />
-                <h2>Marked Data</h2>
-                <div className="marketInfos">
-                  <div className="currentState">
-                    <p><strong>Current Prizes:</strong></p>
-                    <p>${coin.market_data.current_price.usd} </p>
-                    <p>{coin.market_data.current_price.eur}€ </p>
-                    <p>¥{coin.market_data.current_price.jpy} </p>
-                  </div>
-                  <div>
-                    <p><strong>High / Low:</strong> </p>
-                    <p><span id="highest">↑</span> {coin.market_data.high_24h.usd} <span id="lowest">↓</span> {coin.market_data.low_24h.usd}</p>
-                    <p><span id="highest">↑</span> {coin.market_data.high_24h.eur} <span id="lowest">↓</span> {coin.market_data.low_24h.eur}</p>
-                    <p><span id="highest">↑</span> {coin.market_data.high_24h.jpy} <span id="lowest">↓</span> {coin.market_data.low_24h.jpy}</p>
-                  </div>
-                </div>
-                <div className="prizePercent">
-                  <p><strong>Prize Changes in percent:</strong> </p>
-                  <p>24 hours: {Math.round(coin.market_data.price_change_percentage_24h * 100) / 100}% </p>
-                  <p>7 Days: {Math.round(coin.market_data.price_change_percentage_7d * 100) / 100}% </p>
-                  <p>14 Days : {Math.round(coin.market_data.price_change_percentage_14d * 100) / 100}% </p>
-                </div>
-                <hr />
-                <p><strong>Prize Changes in the last 24 hours:</strong> </p>
-                <p>${Math.round(coin.market_data.price_change_24h * 100) / 100} US Dollar</p>
-              </div>
-            </div>
-          ))}
+          <CoinDetails coindID={this.state.coinID} />
         </header>
       </div>
     );
--- src/CoinDetails.tsx
+++ src/CoinDetails.tsx
@@ -4,61 +4,58 @@ import './App.css';
 
 export default class CoinDetails extends React.Component<any, any> {
 
-    constructor(props: any) {
-        super(props);
-        this.state = { coins: [] };
-    }
-
+    coin: string = this.props.coinID;
 
     componentDidMount() {
-        axios.get('https://api.coingecko.com/api/v3/coins')
+        axios.get('https://api.coingecko.com/api/v3/coins/bitcoin')
             .then((res) => {
                 this.setState({ coins: res.data });
+                console.log(this.coin);
             })
     }
 
     render() {
+        if (!this.state) { return null }
+
+        const coin = this.state.coins;
+
         return (
             <div className="CoinDetails" >
-                <header className="App-header">
-                    {this.state.coins.map((coin: any) => (
-                        <div className="coinDetail">
-                            <div key={coin.id}>
-                                <img src={coin.image.large} alt="" />
-                                <h1>{coin.name} ({coin.symbol})</h1>
-                                <hr />
-                                <h2>last update</h2>
-                                <p>{new Date(coin.last_updated).toUTCString()}</p>
-                                <hr />
-                                <h2>Marked Data</h2>
-                                <div className="marketInfos">
-                                    <div className="currentState">
-                                        <p><strong>Current Prizes:</strong></p>
-                                        <p>${coin.market_data.current_price.usd} </p>
-                                        <p>{coin.market_data.current_price.eur}€ </p>
-                                        <p>¥{coin.market_data.current_price.jpy} </p>
-                                    </div>
-                                    <div>
-                                        <p><strong>High / Low:</strong> </p>
-                                        <p><span id="highest">↑</span> {coin.market_data.high_24h.usd} <span id="lowest">↓</span> {coin.market_data.low_24h.usd}</p>
-                                        <p><span id="highest">↑</span> {coin.market_data.high_24h.eur} <span id="lowest">↓</span> {coin.market_data.low_24h.eur}</p>
-                                        <p><span id="highest">↑</span> {coin.market_data.high_24h.jpy} <span id="lowest">↓</span> {coin.market_data.low_24h.jpy}</p>
-                                    </div>
-
-                                </div>
-                                <div className="prizePercent">
-                                    <p><strong>Prize Changes in percent:</strong> </p>
-                                    <p>24 hours: {Math.round(coin.market_data.price_change_percentage_24h * 100) / 100}% </p>
-                                    <p>7 Days: {Math.round(coin.market_data.price_change_percentage_7d * 100) / 100}% </p>
-                                    <p>14 Days : {Math.round(coin.market_data.price_change_percentage_14d * 100) / 100}% </p>
-                                </div>
-                                <hr />
-                                <p><strong>Prize Changes in the last 24 hours:</strong> </p>
-                                <p>${Math.round(coin.market_data.price_change_24h * 100) / 100} US Dollar</p>
+                <div className="coinDetail">
+                    <div key={coin.id}>
+                        <img src={coin.image.large} alt="" />
+                        <h1>{coin.name} ({coin.symbol})</h1>
+                        <hr />
+                        <h2>last update</h2>
+                        <p>{new Date(coin.last_updated).toUTCString()}</p>
+                        <hr />
+                        <h2>Marked Data</h2>
+                        <div className="marketInfos">
+                            <div className="currentState">
+                                <p><strong>Current Prizes:</strong></p>
+                                <p>${coin.market_data.current_price.usd} </p>
+                                <p>{coin.market_data.current_price.eur}€ </p>
+                                <p>¥{coin.market_data.current_price.jpy} </p>
                             </div>
+                            <div>
+                                <p><strong>High / Low:</strong> </p>
+                                <p><span id="highest">↑</span> {coin.market_data.high_24h.usd} <span id="lowest">↓</span> {coin.market_data.low_24h.usd}</p>
+                                <p><span id="highest">↑</span> {coin.market_data.high_24h.eur} <span id="lowest">↓</span> {coin.market_data.low_24h.eur}</p>
+                                <p><span id="highest">↑</span> {coin.market_data.high_24h.jpy} <span id="lowest">↓</span> {coin.market_data.low_24h.jpy}</p>
+                            </div>
+
                         </div>
-                    ))}
-                </header>
+                        <div className="prizePercent">
+                            <p><strong>Prize Changes in percent:</strong> </p>
+                            <p>24 hours: {Math.round(coin.market_data.price_change_percentage_24h * 100) / 100}% </p>
+                            <p>7 Days: {Math.round(coin.market_data.price_change_percentage_7d * 100) / 100}% </p>
+                            <p>14 Days : {Math.round(coin.market_data.price_change_percentage_14d * 100) / 100}% </p>
+                        </div>
+                        <hr />
+                        <p><strong>Prize Changes in the last 24 hours:</strong> </p>
+                        <p>${Math.round(coin.market_data.price_change_24h * 100) / 100} US Dollar</p>
+                    </div>
+                </div>
             </div>
         );
     }
